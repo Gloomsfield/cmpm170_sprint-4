47d73b586ca31d328d89f16df4535958c3c29aae
Nick Marigo <118312264+Nick-Marigo@users.noreply.github.com>
2026-05-20 17:13:54 -0700

    Added Shooting ball and rotation for AD keys

--- Assets/Scripts/Controller/Controller.cs
+++ Assets/Scripts/Controller/Controller.cs
@@ -4,6 +4,13 @@ using UnityEngine.InputSystem;
 public class Controller : MonoBehaviour
 {
 
+    [SerializeField] private ControllerInputHandler controllerInputHandler; 
+
+    void OnShootBall(InputValue value)
+    {
+        //TODO: Add event to shoot ball
+        Debug.Log("ShootBall: " + value.Get<float>());
+    }
 
     void OnLeftPaddle(InputValue value)
     {
@@ -15,6 +22,12 @@ public class Controller : MonoBehaviour
     {
         //TODO: Add event to move right paddle
         Debug.Log("Right Paddle: " + value.Get<float>());
+    }
+
+    void OnRotationAD(InputValue value)
+    {
+        //TODO: Add event to handle rotation AD input
+        Debug.Log("Rotation AD: " + value.Get<float>());
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
