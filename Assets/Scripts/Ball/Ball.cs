e3f032c8e00ef4d04bd559dd4156105a6b4290e7
Nick Marigo <118312264+Nick-Marigo@users.noreply.github.com>
2026-05-22 11:07:50 -0700

    Added tipping feature FOR STEERING WHEEL ONLY

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
+using System.Collections;
 
 public class Ball : MonoBehaviour
 {
-    Rigidbody rigidbody;
+    Rigidbody ballRigidbody;
 
     [Header("Ball Settings")]
     [SerializeField] float shootForce = 10.0f;
     [SerializeField] float rotationForce = 5.0f;
+    [SerializeField] float ballTippingTime = 2.0f;
     float rotation;
+    bool ballTipping = false;
+    bool ballTippedTooMuch = false;
+    Coroutine tippingCoroutine;
 
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        ballRigidbody = GetComponent<Rigidbody>();
     }
 
     void OnEnable()
@@ -33,13 +38,32 @@ public class Ball : MonoBehaviour
     void ShootBall()
     {
         Debug.Log("Ball shot!");
-        rigidbody.AddForce(0, 0, shootForce, ForceMode.Impulse);
+        ballRigidbody.AddForce(0, 0, shootForce, ForceMode.Impulse);
     }
 
     void GetRotation(float rotationValue)
     {
         rotation = rotationValue;
-        Debug.Log("Ball rotated: " + rotationValue);
+
+        if (!ballTipping && (rotationValue < -0.1f || rotationValue > 0.1f))
+        {
+            //Debug.Log("Ball should BE tipping");
+            ballTipping = true;
+            tippingCoroutine = StartCoroutine(BallTipToMuch());
+        }
+        else if (ballTipping && (rotationValue > -0.1f && rotationValue < 0.1f))
+        {
+            //Debug.Log("Ball should not be tipping");
+            ballTipping = false;
+
+            if (tippingCoroutine != null)
+            {
+                StopCoroutine(tippingCoroutine);
+                tippingCoroutine = null;
+            }
+        }
+
+        //Debug.Log("Ball rotated: " + rotationValue);
     }
 
     void GetRotationAD(float rotationValue)
@@ -51,17 +75,31 @@ public class Ball : MonoBehaviour
     void ResetBall()
     {
         Debug.Log("Ball reset!");
-        rigidbody.linearVelocity = Vector3.zero;
+        ballRigidbody.linearVelocity = Vector3.zero;
         transform.position = new Vector3(0, 6, 13);
+        ballTippedTooMuch = false;
     }
 
     void Update()
     {
-        ApplyRotation();
+        if (!ballTippedTooMuch)
+        {
+            ApplyRotation();
+        }
+;
     }
 
     void ApplyRotation()
     {
         transform.Translate(new Vector3(rotation * rotationForce, 0, 0) * Time.deltaTime);
+    }
+
+    IEnumerator BallTipToMuch()
+    {
+        ballTipping = true;
+        yield return new WaitForSeconds(ballTippingTime);
+        // TODO: Change this to a kill pinball function or something that stops all controls and resets the ball after it falls off the table
+        ballTippedTooMuch = true;
+        Debug.Log("Ball tipped too much!");
     }
 }
