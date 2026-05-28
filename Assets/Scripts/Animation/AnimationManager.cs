aba9ab65466c1ea9cfd43486ebeb3fd99e7ac0fe
chimpin <cbonardi@ucsc.edu>
2026-05-27 22:45:37 -0700

    Functions defined in AnimationManager
    defined functions and needed structure for AnimationManager, need to fill them out

--- Assets/Scripts/Animation/AnimationManager.cs
+++ Assets/Scripts/Animation/AnimationManager.cs
@@ -1,6 +1,51 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class AnimationManager : MonoBehaviour
 {
-    
+    [System.Serializable]
+    public class AnimationFrame
+    {
+      
+    }
+
+    [System.Serializable]
+    public class ScreenAnimation
+    {
+       
+    }
+
+    [Header("Screen")]
+    [SerializeField] Renderer screenRenderer;
+
+    [Header("Animations")]
+    [SerializeField] List<ScreenAnimation> animations;
+
+    Dictionary<string, ScreenAnimation> animationDictionary;
+
+    void Awake()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        
+    }
+
+    void OnDisable()
+    {
+       
+    }
+
+    void PlayAnimation(string animationName)
+    {
+
+    }
+
+    IEnumerator PlayAnimationCoroutine(ScreenAnimation animation)
+    {
+
+    }
 }
