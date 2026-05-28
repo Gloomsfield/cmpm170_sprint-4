2a0278c33e7906861ce9817addc69d7e3573c582
chimpin <cbonardi@ucsc.edu>
2026-05-27 23:39:42 -0700

    Screen System working
    Screen is displaying the scenes per animation one after the other but haven't tested audio yet and need to define more audio variables such as delay and duration. I also need to allow multiple screenplayers to be active so we can include the reflection of the beast.

--- Assets/Scripts/Animation/AnimationManager.cs
+++ Assets/Scripts/Animation/AnimationManager.cs
@@ -7,13 +7,16 @@ public class AnimationManager : MonoBehaviour
     [System.Serializable]
     public class AnimationFrame
     {
-      
+        public Texture image;
+        public float duration = 2f;
+        public List<string> soundsToPlay;
     }
 
     [System.Serializable]
     public class ScreenAnimation
     {
-       
+        public string animationName;
+        public List<AnimationFrame> frames;
     }
 
     [Header("Screen")]
@@ -26,26 +29,46 @@ public class AnimationManager : MonoBehaviour
 
     void Awake()
     {
-        
+        animationDictionary = new Dictionary<string, ScreenAnimation>();
+        foreach (var animation in animations)
+        {
+            animationDictionary.Add(animation.animationName, animation);
+        }
     }
 
     void OnEnable()
     {
-        
+        EventManager.playAnimation += PlayAnimation;
     }
 
     void OnDisable()
     {
-       
+        EventManager.playAnimation -= PlayAnimation;
     }
 
     void PlayAnimation(string animationName)
     {
+        if (!animationDictionary.ContainsKey(animationName))
+        {
+            Debug.LogWarning("Animation not found: " + animationName);
+            return;
+        }
 
+        StopAllCoroutines();
+        StartCoroutine(PlayAnimationCoroutine(animationDictionary[animationName]));
     }
 
     IEnumerator PlayAnimationCoroutine(ScreenAnimation animation)
     {
+        foreach (var frame in animation.frames)
+        {
+            screenRenderer.material.mainTexture = frame.image;
+            foreach (var soundName in frame.soundsToPlay)
+            {
+                AudioManager.Instance.PlaySound(soundName);
+            }
 
+            yield return new WaitForSeconds(frame.duration);
+        }
     }
 }
